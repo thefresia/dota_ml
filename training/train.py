a5016d8c5b2d35817b8c8160c6d36e3f4310af43
LFJ <pringelz1337@gmail.com>
2019-08-25 01:48:59 +0300

    clustering

--- training/train.py
+++ training/train.py
@@ -1,9 +1,12 @@
 #%%
 from sklearn.manifold import TSNE
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
 from itertools import repeat
 import numpy as np
 import pymongo
+import mglearn
 from pprint import pprint
 props = {"match_id", "barracks_status_dire", "barracks_status_radiant",
     "dire_score", "radiant_score", "duration", "first_blood_time", 
@@ -25,8 +28,9 @@ learningPlayerProps = { "assists", "deaths",
     "win","isRadiant","kda"}
 playerOnly = { "assists", "deaths",
     "denies","gold_per_min","hero_damage","hero_healing",
-    "hero_id", "kills", "last_hits","tower_damage","xp_per_min",
-    "win","kda"
+    "kills", "last_hits","tower_damage","xp_per_min",
+    "win"
+
 }
 
 def transform(obj, properties):
@@ -78,11 +82,11 @@ def train_hero():
     con = MongoDbConnection.getCollection("dota_ml")
     col = con['data']
     tsne = TSNE(random_state=69)
-    count = 7000*10
+    count = 250*10
     if (count > col.count()*10):
         exit
-    data = np.empty((count,13))
-    original={'heroNames': [None]*200, 'heroes': [None]*count,'tsne_transformed': [np.zeros(3) for i in repeat(None, 200)]}
+    data = np.empty((count,11))
+    original={'heroNames': [None]*130, 'heroes': [None]*count,'tsne_transformed': [np.zeros(3) for i in repeat(None, 130)]}
     for hero in con["heroes"].find():
         original["heroNames"][hero['id']] = hero["localized_name"]
     i = 0
@@ -91,15 +95,19 @@ def train_hero():
         for player in x["players"]:
             if player == {}:
                 continue
+            heroId = player["hero_id"]
             data[i] = np.fromiter(transform(player, playerOnly).values(), dtype=float)
-            original["heroes"][i] = {'name' :original["heroNames"][player["hero_id"]], 'id':player["hero_id"]}
+            original["heroes"][i] = {'name' :original["heroNames"][heroId], 'id':heroId}
             i+=1
             if i == count:
                 break
         if i == count:
             break
+    print('data is prepared.')
+    print('applying tsne.')
     original["data"] = data
     original["tsne"] = tsne.fit_transform(original["data"])
+    print('tsne is finished.')
     res = original["tsne"]
     for i in range(len(res)):
         entry = original["tsne_transformed"][original["heroes"][i]['id']]
@@ -128,11 +136,23 @@ def train_hero():
             ymin = entry[1]
     plt.xlim(xmin, xmax + 1)
     plt.ylim(ymin, ymax + 1)
+    agg = AgglomerativeClustering(n_clusters=10)
+    clusters = agg.fit_predict(res)
+    colors = [ "red", "green", "blue", "purple", "white", "pink", "yellow", "orange", "brown", "grey" ]
+    group = []
+    for c in colors:
+        group.append([])
     for i in range(len(res)):
         if res[i][2] != 0:
-            plt.text(res[i][0], res[i][1], original["heroNames"][i])
+            plt.text(res[i][0], res[i][1], original["heroNames"][i], color=colors[clusters[i]])
+            group[clusters[i]].append(original["heroNames"][i])
     plt.xlabel("t-SNE feature 0")
     plt.xlabel("t-SNE feature 1")
-    print(1)
+    for i in range(len(group)):
+        print('%d.' % i, end = ' ')
+        for hero in group[i]:
+            print(hero, end = ', ')
+        print('\n')
+    print('tis over')
 
 train_hero()
